aae21d1bf0fa31e66f80a0d95deb84e01ed709ef
Andrii Baran <andriy.baran.v@gmail.com>
2018-01-18 15:28:18 +0200

    Added static pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def terms_of_service
+  end
+
+  def privacy_policy
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'terms-of-service', to: 'pages#terms_of_service'
+
+  get 'privacy-policy', to: 'pages#privacy_policy'
+
+  get 'contact', to: 'pages#contact'
+
   resources :runs
   root to: 'visitors#index'
   devise_for :users
